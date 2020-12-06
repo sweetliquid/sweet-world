6e703d202f0745d349f7fd1f2801df3636cafd72
Sweet Liquid <himejisyana@hotmail.com>
2020-12-06 18:28:16 +0800

    Set up Tialwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./page/**/*.[js|jsx|ts|tsx]', './components/**/*.[js|jsx|ts|tsx]'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
