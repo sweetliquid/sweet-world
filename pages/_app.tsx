ba53caf4707c52a66e08f2a67d60d95e1739ff09
Sweet Liquid <himejisyana@hotmail.com>
2020-12-06 18:49:39 +0800

    Add global styles and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components/macro'
 import 'tailwindcss/tailwind.css'
 
+const GlobalStyles = createGlobalStyle``
+const theme = {}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
